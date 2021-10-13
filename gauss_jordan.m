78255b47150145a05e06076543af60fb07210c15
daduam <josephampadu549@gmail.com>
2021-10-13 19:14:42 +0000

    Add Gauss-Jordan elimination

---
+++ gauss_jordan.m
@@ -0,0 +1,31 @@
+function x = gauss_jordan(A, b)
+  dim = length(A);
+  
+  % augment A with b
+  A = [A, b];
+  
+  for j = 1:dim-1
+    for i = 2:dim
+      if A(j,j) == 0
+        [A(1,:), A(i,:)] = deal(A(i,:), A(1,:));
+      endif
+    endfor
+    
+    for i = j+1:dim
+      A(i,:) -= A(j,:) * (A(i,j) / A(j,j));
+    endfor
+  endfor
+  
+  for j = dim:-1:2
+    for i = j-1:-1:1
+      A(i,:) -= A(j,:) * (A(i,j) / A(j,j));
+    endfor
+  endfor
+  
+  for i = 1:dim
+    A(i,:) /= A(i,i);
+  endfor
+
+  % result is the last column of [A|b]
+  x = A(:,dim+1);
+endfunction

21963f6657d3484a92bedf350e5ef6995ed5fbe6
daduam <josephampadu549@gmail.com>
2021-10-13 16:38:39 +0000

    add gaussian elimination without pivoting

---
+++ naive_gaussian.m
@@ -0,0 +1,24 @@
+% TODO make better generally
+% TODO error handling
+% Solve Ax=b using Gaussian elimination without pivoting
+function x = naive_gaussian(A, b)
+  dim = length(A);
+  x = zeros(dim,1)
+  
+  % forward elimination
+  for i = 2:dim
+    m = -A(i:dim,i-1) / A(i-1,i-1);
+    A(i:dim,:) += m * A(i-1,:);
+    b(i:dim,:) += m * b(i-1,:);
+  endfor
+  
+  % backward substitution
+  for i = dim:-1:1
+    if i == dim
+      x(i,:) = b(i,:) / A(i,i);
+    else
+      x(i,:) = b(i,:) - A(i,i+1:dim) * x(i+1:dim,:);
+      x(i,:) /= A(i,i);
+    endif
+  endfor
+endfunction
